4427e651c9de42c4c7a6a7de15b195f688e1327e
Mikita Tsikhanovich <me@mtsikhanovich.tk>
2021-09-16 16:14:00 +0300

    add lab 3

---
+++ OOTP_Lab3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOTP_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train train0 = new Train(5);
+            Train train1 = new Train(1, "Moscow", "10:00", 100, 50, 10);
+            Train train2 = new Train(2, "Kyiv", new TimeSpan(15, 0, 0), 120, 60, 15);
+
+            Console.WriteLine($"train0.Equals(train1): {train0.Equals(train2)}");
+            Console.WriteLine($"train1.ToString(): {train1}");
+            Console.WriteLine($"train2.GetHashCode(): {train2.GetHashCode()}");
+
+            Train[] trains = new Train[6];
+            trains[0] = new Train(3, "Vilnius", "11:25", 100, 100, 10);
+            trains[1] = new Train(4, "Brest", "13:10", 100, 100, 10);
+            trains[2] = new Train(6, "Vitsiebsk", "15:15", 100, 100, 10);
+            trains[3] = new Train(8, "Hrodna", "19:19", 100, 100, 10);
+            trains[4] = new Train(9, "Hrodna", "15:17", 100, 100, 10);
+            trains[5] = new Train(10, "Vitsiebsk", "17:33", 100, 100, 10);
+
+            while (true)
+            {
+                Console.WriteLine("1 — список поездов, следующих до заданного пункта назначения");
+                Console.WriteLine("2 — список поездов, отправляющихся после заданного часа");
+                int choise = Convert.ToInt32(Console.ReadLine());
+                switch (choise)
+                {
+                    case 1:
+                        {
+                            Console.Write("Пункт назначения: ");
+                            string destination = Console.ReadLine();
+                            Console.WriteLine("id\t№\tотправл.\tкупе\tплац.\tVIP");
+                            foreach (Train train in trains)
+                            {
+                                if (train.Destination == destination)
+                                {
+                                    Console.WriteLine($"{train}");
+                                }
+                            }
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.Write("Пункт назначения: ");
+                            string destination = Console.ReadLine();
+                            Console.Write("Минимальный час отправления: ");
+                            TimeSpan departure = new TimeSpan(Convert.ToInt32(Console.ReadLine()), 0, 0);
+                            Console.WriteLine("id\t№\tотправл.\tкупе\tплац.\tVIP");
+                            foreach (Train train in trains)
+                            {
+                                if (train.Destination == destination && departure < train.Departure)
+                                {
+                                    Console.WriteLine($"{train}");
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ OOTP_Lab3/Train.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOTP_Lab3
+{
+    partial class Train
+    {
+
+        private static int instancesCount = 0;
+
+        public static void WriteInfo()
+        {
+            Console.WriteLine($"Количество экземпляров класса Train: {Train.instancesCount}");
+        }
+
+        private int id;
+        private int number;
+        private string destination;
+        private int compartmentSeats;
+        private TimeSpan departure;
+        private int reservedSeats;
+        private int vipSeats;
+
+        public int Number { get => this.number; set => this.number = value; }
+        public string Destination { get => this.destination; set => this.destination = value; }
+        public TimeSpan Departure { get => this.departure; set => this.departure = value; }
+        public int CompartmentSeats { 
+            get => this.compartmentSeats; 
+            set
+            {
+                if (value >= 0 && value < 1000)
+                {
+                    this.compartmentSeats = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        public int ReservedSeats
+        {
+            get => this.reservedSeats;
+            set
+            {
+                if (value >= 0 && value < 1000)
+                {
+                    this.reservedSeats = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        public int VipSeats
+        {
+            get => this.vipSeats;
+            set
+            {
+                if (value >= 0 && value < 1000)
+                {
+                    this.vipSeats = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        public int Seats
+        {
+            get
+            {
+                return this.CompartmentSeats + this.ReservedSeats + this.VipSeats;
+            }
+        }
+
+        private Train() { Console.WriteLine("Private constructor called"); }
+
+        public Train(int number, string destination, string departure, int compartmentSeats = 0, int reservedSeats = 0, int vipSeats = 0)
+        {
+            this.Number = number;
+            this.Destination = destination;
+
+            int[] fractions = departure.Split(':').Select(str => Convert.ToInt32(str)).ToArray();
+            this.Departure = new TimeSpan(fractions[0], fractions[1], 0);
+
+            this.CompartmentSeats = compartmentSeats;
+            this.ReservedSeats = reservedSeats;
+            this.VipSeats = vipSeats;
+
+            this.id = Train.instancesCount;
+            Train.instancesCount++;
+        }
+
+        public Train(int number, string destination, TimeSpan departure, int compartmentSeats = 0, int reservedSeats = 0, int vipSeats = 0)
+        {
+            this.Number = number;
+            this.Destination = destination;
+
+            this.Departure = departure;
+
+            this.CompartmentSeats = compartmentSeats;
+            this.ReservedSeats = reservedSeats;
+            this.VipSeats = vipSeats;
+
+            this.id = Train.instancesCount;
+            Train.instancesCount++;
+        }
+
+        public Train(int number)
+        {
+            this.Number = number;
+            this.Destination = "";
+            this.Departure = new TimeSpan(0, 0, 0);
+            this.CompartmentSeats = 0;
+            this.ReservedSeats = 0;
+            this.VipSeats = 0;
+
+            this.id = Train.instancesCount;
+            Train.instancesCount++;
+        }
+
+        public override bool Equals(Object anotherObject)
+        {
+            return anotherObject.GetType() == this.GetType() && anotherObject.GetHashCode() == this.GetHashCode();
+
+        }
+
+        public override int GetHashCode()
+        {
+            return this.id;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.id}\t{this.number}\t{this.departure}\t{this.compartmentSeats}\t{this.reservedSeats}\t{this.vipSeats}";
+        }
+
+    }
+}
